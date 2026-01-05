cfde2e93fcdff01623efd07847052e2571306d97
alerubira <100098255+alerubira@users.noreply.github.com>
2026-01-05 18:32:26 -0300

    create personas controller crate post

--- Controllers/PersonasController.cs
+++ Controllers/PersonasController.cs
@@ -7,15 +7,18 @@ using System.Security.Claims;
 using Juego.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace Juego.Controllers
 {
     public class PersonasController : Controller
     {
           private readonly DataContext _context;
+          private readonly SeguridadService _seguridadService;
 
-        public PersonasController(DataContext context)
+        public PersonasController(DataContext context, SeguridadService seguridadService)
         {
             _context = context;
+            _seguridadService = seguridadService;
         }
 
         [HttpGet]
@@ -36,13 +39,32 @@ namespace Juego.Controllers
         public async Task<IActionResult> Create(Personas persona)
         {
 
-            if (ModelState.IsValid)
-            {
-                _context.Add(persona);
+             if (!ModelState.IsValid)
+                    return View(persona);
+
+                // 1️⃣ Verificar si existe el email
+                bool emailExiste = await _context.Personas
+                    .AnyAsync(p => p.Email == persona.Email);
+
+                if (emailExiste)
+                {
+                    ModelState.AddModelError("Email", "El correo ya está registrado");
+                    return View(persona);
+                }
+
+                // 2️⃣ Asignar rol por defecto
+                persona.IdRol = 1;
+
+                // 3️⃣ Datos del sistema
+                persona.Existe = true;
+                persona.Clave = _seguridadService.HashearContraseña(persona.Clave);
+
+                // 4️⃣ Guardar
+                _context.Personas.Add(persona);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(persona);
+
+                // 5️⃣ Redirigir al login
+                return RedirectToAction("Index", "Login");
         }
 }
 }
