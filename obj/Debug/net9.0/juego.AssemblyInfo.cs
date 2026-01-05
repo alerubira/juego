298d19e8232a75da8cbff0cbffaf948a7fb334b4
alerubira <100098255+alerubira@users.noreply.github.com>
2026-01-05 20:14:33 -0300

    create preguntajuegoModels

--- obj/Debug/net9.0/juego.AssemblyInfo.cs
+++ obj/Debug/net9.0/juego.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("juego")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91906d62fca8a4910615406b2f5dc1320d7aa726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cbe426b787b75279f10f55e9399204afa94fc7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("juego")]
 [assembly: System.Reflection.AssemblyTitleAttribute("juego")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
