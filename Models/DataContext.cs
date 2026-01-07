ba6fb9f86e0ffd819696ea9ed0e88aa75fe9d17c
alerubira <100098255+alerubira@users.noreply.github.com>
2026-01-07 10:59:35 -0300

    Update DataContext.cs
    agregado para borrar en cascada lapregunta con sus respuestas

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -14,5 +14,15 @@ namespace Juego.Models
         public DbSet<Personas> Personas { get; set; }
         public DbSet<Preguntas> Preguntas { get; set; }
         public DbSet<Respuestas> Respuestas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Respuestas>()
+                .HasOne(r => r.Pregunta)
+                .WithMany(p => p.Respuestas)
+                .HasForeignKey(r => r.IdPregunta)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
